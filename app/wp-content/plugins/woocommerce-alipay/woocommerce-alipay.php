686db2cdfbaf9a35c3eb0ca3c4874625129a0596
michael <zdong2010@gmail.com>
2019-09-22 16:36:07 +0800

    添加插件/woocommerce

---
+++ app/wp-content/plugins/woocommerce-alipay/woocommerce-alipay.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: WooCommerce Alipay Payment Gateway
+Plugin URI: https://ninghao.net
+Description: Alipay payment gatway for WooCommerce
+Version: 1.0.0
+Author: michael
+Auhtor URI: https://ninghao.net
+*/ 
+
+if( !defined('ABSPATH')) {
+    exit;
+}
